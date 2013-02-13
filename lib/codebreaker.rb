f9a91eaec492109e0117c497c3cd7045f06c2a94
Artem Suchov <artem.suchov@gmail.com>
2013-02-13 22:49:51 +0200

    require_reletive

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,14 +1,8 @@
-#require 'codebreaker/version'
-#require 'codebreaker/game'
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require_relative 'codebreaker/version'
+require_relative 'codebreaker/game'
 
 
-require 'codebreaker/game'
-
-game = Codebreaker::Game.new(STDOUT)
-game.start
-#module Codebreaker
-  # Your code goes here..
-#game = Game.new
-#game.start
-#end
+module Codebreaker
+  game = Game.new(STDOUT)
+  game.start
+end
