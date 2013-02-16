669b675d2b96637168f8daf84859dd56633b9512
Artem Suchov <artem.suchov@gmail.com>
2013-02-16 08:55:40 +0200

    specify

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,83 +1,83 @@
-require 'codebreaker/version'
+#require 'codebreaker/version'
 
 module Codebreaker
-    class Game
-        def initialize(output = STDOUT)
-            @output = output
-        end
+class Game
+    def initialize(output = STDOUT)
+        @output = output
+    end
+    
+    def start
+        @secret = generate_code
+        @output.puts "Welcome to Codebreaker! \n To see the hint press h and ENTER \n To exit press q and ENTER"
         
-        def start
+        prompt
+    end
+    
+    def prompt
+        @output.puts 'Enter guess:'
+        answer = gets.chomp
+        case answer
+            when 'h'
+            hint
+            when 'q'
+            exit
+            else
+            guess(answer)
+        end
+        prompt unless answer.nil? or answer == ""
+    end
+    
+    def hint
+        @output.puts "The first number is: #{@secret}"
+    end
+    
+    def guess(guess)
+        marker = Marker.new(@secret, guess)
+        if marker.exact_match_count != 4
+            @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
+            else
+            @output.puts "YOU WIN THE GAME \n Just keep playing or type q & ENTER to quit"
             @secret = generate_code
-            @output.puts "Welcome to Codebreaker! \n To see the hint press h and ENTER \n To exit press q and ENTER"
-            
-            prompt
-        end
-        
-        def prompt
-            @output.puts 'Enter guess:'
-            answer = gets.chomp
-            case answer
-                when 'h'
-                hint
-                when 'q'
-                exit
-                else
-                guess(answer)
-            end
-            prompt unless answer.nil? or answer == ""
-        end
-        
-        def hint
-            @output.puts "The first number is: #{@secret}"
-        end
-        
-        def guess(guess)
-            marker = Marker.new(@secret, guess)
-            if marker.exact_match_count != 4
-                @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
-                else
-                @output.puts "YOU WIN THE GAME \n Just keep playing or type q & ENTER to quit"
-                @secret = generate_code
-            end
-        end
-        
-        private
-        def generate_code
-            @code = Array('1'..'6').sample(4).join
         end
     end
     
-    class Marker
-        def initialize(secret, guess)
-            @secret, @guess = secret, guess
-        end
-        
-        def number_match_count
-            total_match_count - exact_match_count
-        end
-        
-        def total_match_count
-            secret = @secret.split('')
-            @guess.split('').inject(0) do |count, n|
-                count + (delete_first(secret, n) ? 1 : 0)
-            end
-        end
-        def delete_first(code, n)
-            code.delete_at(code.index(n)) if code.index(n)
-        end
-        def exact_match_count
-            (0..3).inject(0) do |count, index|
-                count + (exact_match?(@guess, index) ? 1 : 0)
-            end
-        end
-        
-        def exact_match?(guess, index)
-            @guess[index] == @secret[index]
-        end
-        
-        def number_match?(guess, index)
-            @secret.include?(@guess[index]) && !exact_match?(@guess, index)
+    private
+    def generate_code
+        @code = Array('1'..'6').sample(4).join
+    end
+end
+
+class Marker
+    def initialize(secret, guess)
+        @secret, @guess = secret, guess
+    end
+    
+    def number_match_count
+        total_match_count - exact_match_count
+    end
+    
+    def total_match_count
+        secret = @secret.split('')
+        @guess.split('').inject(0) do |count, n|
+            count + (delete_first(secret, n) ? 1 : 0)
         end
     end
+    def delete_first(code, n)
+        code.delete_at(code.index(n)) if code.index(n)
+    end
+    def exact_match_count
+        (0..3).inject(0) do |count, index|
+            count + (exact_match?(@guess, index) ? 1 : 0)
+        end
+    end
+    
+    def exact_match?(guess, index)
+        @guess[index] == @secret[index]
+    end
+    
+    def number_match?(guess, index)
+        @secret.include?(@guess[index]) && !exact_match?(@guess, index)
+    end
+end
 end
 #Codebreaker::Game.new.start
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -21,22 +21,18 @@ describe "#ptomtp" do
     end
     
     context "when press h " do
-        it "mus be only one hint" do
-            pending
-        end
+          pending "fuck"
     end
     
     context "when win" do
-        it "shold say..." do
             pending "make congratulations"
-        end
     end
     
-    it "when y - play again " do
+    context "when y - play again " do
         pending "make y in play again"
     end
     
-    it "when n - must be quit" do
+    context "when n - must be quit" do
         pending "make quit for n"
     end
 end
@@ -44,64 +40,47 @@ end
 describe Marker do
     describe "#exact_match_coutn" do
         context "with no matches" do
-            it "returns 0" do
                 marker = Marker.new('1234', '6666')
-                marker.exact_match_count.should == 0
-            end
+                specify {marker.exact_match_count.should == 0}
         end
         
         context "with 1 extra match" do
-            it "return 1" do
                 marker = Marker.new('1234', '1666')
-                marker.exact_match_count.should == 1
-            end
+                specify {marker.exact_match_count.should == 1}
+            
         end
         
         context "with 1 number match" do
-            it "return 0" do
                 marker = Marker.new('1234', '2555')
-                marker.exact_match_count.should == 0
-            end
+                specify {marker.exact_match_count.should == 0}
         end
         
         context "with 1 extra match and 1 num match" do
-            it "returs 1" do
                 marker = Marker.new('1234', '1525')
-                marker.exact_match_count.should == 1
-            end
+                specify {marker.exact_match_count.should == 1}
         end
     end
     describe "#number match coutn" do
         context "with 1 extra match duplicated in guess" do
-            it "returns 0" do
                 marker = Marker.new('1234', '1155')
-                marker.number_match_count.should == 0
-            end
+                specify {marker.number_match_count.should == 0}
         end
         
         context "with no matches" do
-            it "returs 0" do
                 marker = Marker.new('1234', '6666')
-                marker.number_match_count.should == 0
-            end
+                specify {marker.number_match_count.should == 0}
         end
         context "with 1 number match" do
-            it "returns 1" do
                 marker = Marker.new('1234', '6661')
-                marker.number_match_count.should == 1
-            end
+                specify {marker.number_match_count.should == 1}
         end
         context "with 1 exact match do" do
-            it "returns 0" do
                 marker = Marker.new('1234', '1555')
-                marker.number_match_count.should == 0
-            end
+                specify {marker.number_match_count.should == 0}
         end
         context "with 1 extra match and 1 number march" do
-            it "returns 1" do
                 marker = Marker.new('1234', '1525')
-                marker.number_match_count.should == 1
-            end
+                specify {marker.number_match_count.should == 1}
         end
     end
 end
